b5973e59236fbf2cd13829c1cb781eb565690558
Greg Kaczorek <grzegorz.kaczorek@gmail.com>
2014-01-14 15:06:22 +0100

    Fix model logic

--- spec/models/achiever_spec.rb
+++ spec/models/achiever_spec.rb
@@ -22,64 +22,66 @@ describe Achiever do
 
   describe "when changing points" do
     it "should have 0 point at start" do
-      expect(@achiever.points).to be 0
+      expect(@achiever.points).to eq 0
     end
    
     it "should properly add points when new" do
       @achiever.change_points(10)
-      expect(@achiever.points).to be 10
+      expect(@achiever.points).to eq 10
     end 
 
     it "should properly add points when points exist" do
       @achiever.change_points(10)
       @achiever.change_points(5)
-      expect(@achiever.points).to be 15
+      expect(@achiever.points).to eq 15
     end
   end
 
   describe "when achieving stuff" do
     it "should gain points for every achievement" do
-      expect(@achiever.points).to be 0
+      expect(@achiever.points).to eq 0
      
       achievement = FactoryGirl.create(:achievement)
       @achiever.use(achievement)
-      expect(@achiever.points).to be 1 
+      expect(@achiever.points).to eq 1 
     end
   end
 
   describe "when being rewarded" do
     it "should substract points for every reward given" do
       achiever = FactoryGirl.create(:hundred_point_achiever)
-      expect(achiever.points).to be 100
+      expect(achiever.points).to eq 100
      
       reward = FactoryGirl.create(:ten_point_reward)
       achiever.use(reward)
 
-      expect(achiever.points).to be 90
+      expect(achiever.points).to eq 90
     end
   end
   describe "when being punished" do
     it "should substract points for every punishment" do
       achiever = FactoryGirl.create(:hundred_point_achiever)
-      expect(achiever.points).to be 100
+      expect(achiever.points).to eq 100
      
       punishment =  FactoryGirl.create(:ten_point_punisher)
       achiever.use(punishment)
 
-      expect(achiever.points).to be 90
+      expect(achiever.points).to eq 90
     end
   end
   
   describe "when given something with and older ts that was completed before its creation" do
     it "should get achiever points from creation date, not ts" do
       achiever = FactoryGirl.create(:zero_point_achiever)
-      expect(achiever.points).to be 0
+      expect(achiever.points).to eq 0
       
-      ts_five_minutes_ago_data = { created_at: Time.now()+10, ts: Time.now()-300 }
+      ts_five_minutes_later_data = { created_at: Time.now()-10, ts: Time.now()+300 }
       achiever.add_status_change(FactoryGirl.create(:ten_points_now, { points_after: 10})) 
-      achiever.add_status_change(FactoryGirl.create(:ten_points_now,ts_five_minutes_ago_data))
+      achiever.add_status_change(FactoryGirl.create(:ten_points_now,ts_five_minutes_later_data))
+      
+      binding.pry
 
-      expect(achiever.points).to be 10
+      expect(achiever.points).to eq 10
     end
   end
 end
